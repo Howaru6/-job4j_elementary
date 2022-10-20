d3694328ce783598c68a9ef4cc783ab99b1bbd42
Howaru <Hellwudpecker1707@gmail.com>
2022-10-20 20:00:57 +0300

    4.2. Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[]args) {
+        System.out.println("Igor Kadochnikov");
+        System.out.println("05.04.1998");
+
+    }
+}
