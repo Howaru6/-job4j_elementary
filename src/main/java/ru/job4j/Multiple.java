fb4aca1d78bb399df71456fe05b98a4f62791b0a
Howaru <Hellwudpecker1707@gmail.com>
2022-10-20 20:01:52 +0300

    6. Checkstyle [#4123]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+
+        System.out.println(1 * 1);
+        System.out.println(1 * 2);
+        System.out.println(1 * 3);
+        System.out.println(1 * 4);
+        System.out.println(1 * 5);
+        System.out.println(1 * 6);
+        System.out.println(1 * 7);
+        System.out.println(1 * 8);
+        System.out.println(1 * 9);
+        System.out.println(1 * 10);
+    }
+}
