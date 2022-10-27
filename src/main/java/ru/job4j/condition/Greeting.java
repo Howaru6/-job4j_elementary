fe15aa9f965f08e4dfcd7c6a74d962f3ce2e74d8
Howaru <Hellwudpecker1707@gmail.com>
2022-10-27 17:39:41 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like java!";
+        System.out.println(idea + " But i am a newbie");
+        int year = 2022;
+        idea += year;
+        System.out.println(idea);
+    }
+}
