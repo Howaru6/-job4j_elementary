d93aa0ba49cd61b9dea5e52e52da603d58264125
Howaru <Hellwudpecker1707@gmail.com>
2022-11-12 16:58:28 +0300

    5.5. Простое число [#171694 #360002]

---
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = 2; number <= finish; number++) {
+            if (number % number == 0 && number % 1 == 0) {
+                    if (CheckPrimeNumber.check(number)) {
+                        count++;
+                    }
+                }
+        }
+        return count;
+    }
+}
