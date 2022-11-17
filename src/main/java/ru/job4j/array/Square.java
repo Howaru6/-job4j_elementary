36adea35a067f7b1d13aedffb170f2403530ed6b
Howaru <Hellwudpecker1707@gmail.com>
2022-11-17 20:29:15 +0300

    6.0.7. Массивы в тестах. [#226789]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -10,7 +10,7 @@ public class Square {
     }
 
         public static void main(String[]args) {
-            int[] array = calculate(4);
+            int[] array = calculate(5);
             for (int index = 0; index < array.length; index++) {
                 System.out.println(array[index]);
             }
