343726c299708d347e284190081c5df9f2035e8f
Howaru <Hellwudpecker1707@gmail.com>
2022-11-18 16:16:38 +0300

    6.0.6. Заполнить массив степенями чисел. [#33488]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -3,14 +3,14 @@ package ru.job4j.array;
 public class Square {
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
-        for (int index = 0; index < rst.length; index++) {
+        for (int index = 0; index < bound; index++) {
             rst[index] = index * index;
         }
         return rst;
     }
 
         public static void main(String[]args) {
-            int[] array = calculate(5);
+            int[] array = calculate(4);
             for (int index = 0; index < array.length; index++) {
                 System.out.println(array[index]);
             }
