1d7748ad30893c2ad2f08537e3317b8c4e382bb2
Howaru <Hellwudpecker1707@gmail.com>
2022-11-24 16:58:01 +0300

    6.5.1. Поиск минимального числа в массиве. [#179803]

--- src/main/java/ru/job4j/array/Min.java
+++ src/main/java/ru/job4j/array/Min.java
@@ -1,7 +1,6 @@
 package ru.job4j.array;
 
 public class Min {
-
     public static int findMin(int[] array) {
         int min = array[0];
         for (int index = 1; index < array.length; index++) {
--- src/test/java/ru/job4j/array/MinTest.java
+++ src/test/java/ru/job4j/array/MinTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MinTest {
-
     @Test
     public void whenFirstMin() {
         int[] array = new int[] {0, 5, 10};
