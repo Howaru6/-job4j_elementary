a0de5c84d5f8ff2ce5976a92371a649e85c6ea26
Howaru <Hellwudpecker1707@gmail.com>
2022-11-25 17:22:35 +0300

    6.4.1. Сравнить последние элементы двух массивов [#304228 #363854]

--- src/main/java/ru/job4j/array/EqLast.java
+++ src/main/java/ru/job4j/array/EqLast.java
@@ -2,9 +2,7 @@ package ru.job4j.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        for (int i = 0; i < left.length; i++) {
             return left[left.length - 1] == right[right.length - 1];
         }
-        return false;
     }
-}
+
--- src/test/java/ru/job4j/array/EqLastTest.java
+++ src/test/java/ru/job4j/array/EqLastTest.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+
 public class EqLastTest {
     @Test
     public void whenEq() {
