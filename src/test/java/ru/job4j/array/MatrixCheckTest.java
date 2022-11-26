d90964266148789a09d5b76c53eb59a78202bf70
Howaru <Hellwudpecker1707@gmail.com>
2022-11-26 12:47:59 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -2,7 +2,6 @@ package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-
 public class MatrixCheckTest {
     @Test
     public void whenHasMonoHorizontal() {
