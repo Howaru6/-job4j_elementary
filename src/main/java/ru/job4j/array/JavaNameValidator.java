dcb28793384250200a8e97d08cc37241eecbb059
Howaru <Hellwudpecker1707@gmail.com>
2022-12-09 17:53:16 +0300

    6.9. String - это массив [#504884]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,10 +2,10 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        if (!name.isEmpty() && isUpperLatinLetter(name.charAt(0)) && !Character.isDigit(name.charAt(0))) {
+        if (name.isEmpty() || isUpperLatinLetter(name.charAt(0))) {
             for (int i = 1; i < name.length(); i++) {
                 int code = name.codePointAt(i);
-                if (Character.isDigit(code) || isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code)) {
+                if (isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code)) {
                     return true;
                 }
             }
@@ -14,14 +14,14 @@ public class JavaNameValidator {
     }
 
     public static boolean isSpecialSymbol(int code) {
-        return code == 36 || code == 95;
+        return (code == 36 || code == 95) && !Character.isDigit(code);
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return code <= 65 || code >= 90;
+        return (code <= 65 || code >= 90) && !Character.isDigit(code);
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return code <= 97 || code >= 122;
+        return (code <= 97 || code >= 122) && !Character.isDigit(code);
     }
 }
